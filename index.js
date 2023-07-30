16719f34955f65810f695ffce8c7b6ddc7055543
zh10034 <zh10034@LAPTOP-KH5314B2.localdomain>
2023-07-30 15:43:36 -0400

    complete lab

---
+++ index.js
@@ -0,0 +1,15 @@
+function introduction(Aki) {
+    return `Hi, my name is ${Aki}.`
+}
+
+function introductionWithLanguage(Aki, Ember) {
+    return `Hi, my name is ${Aki} and I am learning to program in ${Ember}.`
+}
+
+function introductionWithLanguageOptional(Gracie, language = "JavaScript") { 
+    return `Hi, my name is ${Gracie} and I am learning to program in ${language}.`
+}
+
+
+
+
